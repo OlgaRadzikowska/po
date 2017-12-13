d3d7b622af4c380c45b53131353f8ffdbbfe32fe
Olga Radzikowska <204983@edu.p.lodz.pl>
2017-12-13 17:23:08 +0100

    Utworzenie konstruktorów

--- rcMatrix.h
+++ rcMatrix.h
@@ -2,7 +2,8 @@
 #include <fstream>
 using namespace std;
  
-
+class OutOfMem{};
+ 
 class CMatrix{
 	private:
 	 
@@ -34,6 +35,78 @@ struct CMatrix::rcmatrix
         unsigned int rows;
         unsigned int cols;
         unsigned int n;
+		
+		rcmatrix(){
+                this->data=NULL;
+                this->cols=0;
+                this->rows=0;
+                this->n=0;
+        }
+ 
+		
+        rcmatrix(unsigned int nrows, unsigned int ncols, double var){
+             try
+             {
+                this->data = new double*[nrows];
+                for(unsigned int i=0;i<nrows;i++)
+                this->data[i]=new double[ncols];
+             }
+             catch(...){
+				throw OutOfMem();
+             }
+                 
+            for(unsigned int i=0;i<nrows;i++)
+            for(unsigned int j=0;j<ncols;j++)
+            if(i==j)this->data[i][j]=var;
+            else this->data[i][j]=0.0;
+                 
+            this->rows=nrows;
+            this->cols=ncols;
+            this->n=1;
+        }
+ 
+        rcmatrix(unsigned int nrows, unsigned int ncols, double var1, double var2){
+			try
+            {
+                this->data = new double*[nrows];
+                for(unsigned int i=0;i<nrows;i++)
+                this->data[i]=new double[ncols];    
+            }
+			catch(...){
+				throw OutOfMem(); 
+			}
+                         
+			for(unsigned int i=0;i<nrows;i++)
+			for(unsigned int j=0;j<ncols;j++)
+			if(i==j)this->data[i][j]=var1;
+			else this->data[i][j]=var2;
+                         
+			this->rows=nrows;
+			this->cols=ncols;
+			this->n=1;	
+        }
+		
+		rcmatrix(fstream& fs){
+			fs >> this->rows;
+			fs >> this->cols;
+	  
+			try
+			{
+				this->data = new double*[this->rows];
+				for(unsigned int i=0;i<this->rows;i++)
+				this->data[i]=new double[this->cols];
+			}
+			catch(...)
+			{
+				throw OutOfMem();
+			}
+	  
+			for(unsigned int i=0;i<this->rows;i++)
+			for(unsigned int j=0;j<this->cols;j++){
+			fs >> this->data[i][j];
+			}
+			this->n=1;
+		}
  
 };
  
